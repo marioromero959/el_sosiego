aa64a4a3961df37749ebed6379266829e8bf8d60
marioromwb <marioromwb@hotmail.com>
2025-06-22 08:47:25 -0300

    env prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,12 @@
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://localhost:1337/api',
+//   strapiUrl: 'http://localhost:1337',
+//   publicKey: 'APP_USR-963870a8-6d1f-44ef-a5cc-0fb5bdeb36f8',
+// };
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:1337/api',
-  strapiUrl: 'http://localhost:1337',
+  apiUrl: 'https://el-sosiego-back.onrender.com/api',
+  strapiUrl: 'https://el-sosiego-back.onrender.com',
   publicKey: 'APP_USR-963870a8-6d1f-44ef-a5cc-0fb5bdeb36f8',
 };
